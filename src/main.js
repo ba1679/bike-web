8ea08455d2c986ace849c248ed00e4094d7ed0ca
hsinhui <shinhuei1@gmail.com>
2021-11-14 22:17:19 +0800

    plugin - 安裝地圖套件

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'bootstrap' // Import js file
+import 'bootstrap'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-Vue.use(VueAxios, axios) // 套件使用use
+
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+Vue.use(VueAxios, axios)
+Vue.component('LMap', LMap)
+Vue.component('LTileLayer', LTileLayer)
+Vue.component('LMarker', LMarker)
 
 Vue.config.productionTip = false
 
