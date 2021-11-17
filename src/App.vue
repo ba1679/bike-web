17bd1964ef9ef007b8f3d700300888a2a77e26ff
hsinhui <shinhuei1@gmail.com>
2021-11-17 15:46:17 +0800

    設定 loading 狀態資料、判斷斷點資料

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,59 @@
 <template>
-  <div id="app">
+  <div id="app" class="sitcky-footer">
     <Navbar />
+    <loading-overlay :active.sync="isLoading" style="z-index: 1100" />
     <router-view />
-    <Footer />
+    <Footer v-if="showFooter" class="sticky-bottom" />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'App',
   components: {
     Navbar,
     Footer
+  },
+  computed: {
+    ...mapGetters({
+      isLoading: 'isLoading',
+      breakPoint: 'breakPoint'
+    }),
+    showFooter () {
+      if (this.$route.name === 'Route' || this.$route.name === 'Station') {
+        return false
+      } else {
+        return true
+      }
+    }
+  },
+  methods: {
+    detectWindowWidth () {
+      this.$store.dispatch('setBreakpoint', window.innerWidth)
+    }
+  },
+  created () {
+    this.detectWindowWidth()
+    window.addEventListener('resize', this.detectWindowWidth)
+  },
+  unmounted () {
+    window.removeEventListener('resize', this.detectWindowWidth)
+  },
+  watch: {
+    breakPoint: {
+      immediate: true,
+      handler (val) {
+        if (val <= 414) {
+          this.$store.dispatch('setIsMobile', true)
+        } else {
+          this.$store.dispatch('setIsMobile', false)
+        }
+      }
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,78 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import cities from '@/assets/cities.json'
+import JsSHA from 'jssha'
+
+// module
+import station from './station.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isLoading: false,
+    breakPoint: null,
+    isMobile: false,
+    cities: cities,
+    apiHeader: null
+  },
+  getters: {
+    isLoading (state) {
+      return state.isLoading
+    },
+    breakPoint (state) {
+      return state.breakPoint
+    },
+    isMobile (state) {
+      return state.isMobile
+    }
   },
   mutations: {
+    SET_IS_LOADING (state, isLoading) {
+      state.isLoading = isLoading
+    },
+    SET_BREAKPOINT (state, breakPoint) {
+      state.breakPoint = breakPoint
+    },
+    SET_IS_MOBILE (state, isMobile) {
+      state.isMobile = isMobile
+    },
+    SET_API_HEADER (state, header) {
+      state.apiHeader = header
+    }
   },
   actions: {
+    setIsLoading ({ commit }, isLoading) {
+      commit('SET_IS_LOADING', isLoading)
+    },
+    setBreakpoint ({ commit }, breakPoint) {
+      commit('SET_BREAKPOINT', breakPoint)
+    },
+    setIsMobile ({ commit }, isMobile) {
+      commit('SET_IS_MOBILE', isMobile)
+    },
+    getAuthorizationHeader ({ commit }) {
+      return new Promise((resolve) => {
+        const GMTString = new Date().toGMTString()
+        const ShaObj = new JsSHA('SHA-1', 'TEXT')
+        ShaObj.setHMACKey(process.env.VUE_APP_APIAPPKEY, 'TEXT')
+        ShaObj.update('x-date: ' + GMTString)
+        const HMAC = ShaObj.getHMAC('B64')
+        const Authorization =
+          'hmac username="' +
+          process.env.VUE_APP_APIAPPID +
+          '", algorithm="hmac-sha1", headers="x-date", signature="' +
+          HMAC +
+          '"'
+        commit('SET_API_HEADER', {
+          Authorization: Authorization,
+          'X-Date': GMTString
+        })
+        resolve()
+      })
+    }
   },
   modules: {
+    station
   }
 })
