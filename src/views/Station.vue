66a69367dc2f28a1b351678236be32dda13ca529
hsinhui <shinhuei1@gmail.com>
2021-11-17 15:44:11 +0800

    feat - 整合搜尋、選單篩選車站資料功能

--- src/views/Station.vue
+++ src/views/Station.vue
@@ -1,17 +1,58 @@
 <template>
-  <!-- 1. 預設地圖會預設顯示臺北市區域，並且預設顯示借車的所有站牌
-2. 進行站牌搜尋後，地圖會限縮到該範圍
-3. 點選圖標會popup顯示詳細資訊
-4. 上方借車還車btn會切換圖標的資訊，借車：顯示剩餘車輛/還車：顯示剩餘空位的資訊 -->
   <div class="container-fluid position-relative">
     <LMap
       id="map"
       :center="center"
       :zoom="zoom"
-      class="w-100"
-      style="height: calc(100vh - 100px)"
+      class="w-100 map-height"
+      :maxZoom="19"
     >
       <LTileLayer :options="layerOptions" :tile-layer-class="tileLayerClass" />
+      <v-marker-cluster>
+        <LMarker
+          v-for="(marker, index) in markerData"
+          :key="index"
+          :lat-lng="marker.position"
+        >
+          <LIcon
+            :icon-size="layerOptions.iconSize"
+            :icon-anchor="layerOptions.iconAnchor"
+            :icon-url="
+              iconColor(
+                marker.serviceStatus,
+                marker.rentIcon,
+                marker.returnIcon
+              )
+            "
+          />
+          <LPopup :options="{ minWidth: 350 }">
+            <div class="card popup">
+              <div class="card-body">
+                <h3>{{ marker.name }}</h3>
+                <p>{{ marker.address }}</p>
+                <p>更新時間 : {{ marker.updateTime }}</p>
+                <div class="container">
+                  <div class="row">
+                    <template v-if="marker.serviceStatus === 1">
+                      <div class="col info-card mr-1" :class="marker.rentIcon">
+                        <p class="font-weight-bold">可借車輛</p>
+                        <p class="h2 my-0">{{ marker.avaRent }}</p>
+                      </div>
+                      <div class="col info-card" :class="marker.returnIcon">
+                        <p class="font-weight-bold">可停空位</p>
+                        <p class="h2 my-0">{{ marker.avaReturn }}</p>
+                      </div>
+                    </template>
+                    <div class="col info-card bg-gray-300" v-else>
+                      <p class="font-weight-bold">停止/暫停營運</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </LPopup>
+        </LMarker>
+      </v-marker-cluster>
     </LMap>
     <div class="card search-card">
       <div class="card-body">
@@ -22,36 +63,53 @@
               type="text"
               class="form-control font-weight-light"
               placeholder="請輸入站牌關鍵字"
+              v-model="keyWord"
             />
           </div>
-          <div class="d-flex">
-            <div class="form-group mr-3">
-              <label for="city">縣市</label>
-              <select id="city" class="form-control custom-select select-icon">
-                <option selected>Choose...</option>
-                <option>...</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <label for="district">鄉鎮區</label>
-              <select
-                id="district"
-                class="form-control custom-select select-icon"
+          <div class="form-group">
+            <label for="city">縣市</label>
+            <select
+              id="city"
+              class="form-control custom-select select-icon font-weight-light"
+              v-model="citySelect"
+            >
+              <option selected disabled :value="null">請選擇縣市</option>
+              <option
+                v-for="city of cities"
+                :key="city.CityName"
+                :value="city.CityEngName"
               >
-                <option selected>Choose...</option>
-                <option>...</option>
-              </select>
-            </div>
+                {{ city.CityName }}
+              </option>
+            </select>
           </div>
         </form>
-        <button class="btn btn-primary btn-block btn-sm" type="button">
+        <button
+          class="btn btn-primary btn-block btn-sm"
+          type="button"
+          @click="loadCityStationsData"
+        >
           搜尋
         </button>
       </div>
     </div>
     <div class="d-flex rent-btn">
-      <button class="btn btn-black mr-2" type="button">借車</button>
-      <button class="btn btn-gray-200" type="button">還車</button>
+      <button
+        class="btn btn-black mr-2"
+        :class="{ 'btn-gray-300': serviceSelect !== 'rent' }"
+        type="button"
+        @click="serviceSelect = 'rent'"
+      >
+        借車
+      </button>
+      <button
+        class="btn btn-black"
+        :class="{ 'btn-gray-300': serviceSelect !== 'return' }"
+        type="button"
+        @click="serviceSelect = 'return'"
+      >
+        還車
+      </button>
     </div>
   </div>
 </template>
@@ -60,20 +118,70 @@ import L from 'leaflet'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl-leaflet'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import 'leaflet/dist/leaflet.css'
+import { mapState, mapGetters } from 'vuex'
+import { format } from 'date-fns'
 window.mapboxgl = mapboxgl
 
 export default {
   name: 'Station',
-  components: {
-    // LMarker,
-    // LPopup,
-    // LTooltip
+  computed: {
+    ...mapState('station', ['noData']),
+    ...mapGetters({
+      stationsData: 'station/stationsAndAvailability'
+    }),
+    center () {
+      return this.markerData[0]?.position || [25.0408578889, 121.567904444]
+    }
   },
   data () {
     return {
-      center: [25.06, 121.31],
-      zoom: 13,
+      cities: [
+        {
+          CityName: '臺中市',
+          CityEngName: 'Taichung'
+        },
+        {
+          CityName: '新竹市',
+          CityEngName: 'Hsinchu'
+        },
+        {
+          CityName: '苗栗縣',
+          CityEngName: 'MiaoliCounty'
+        },
+        {
+          CityName: '新北市',
+          CityEngName: 'NewTaipei'
+        },
+        {
+          CityName: '屏東縣',
+          CityEngName: 'PingtungCounty'
+        },
+        {
+          CityName: '金門縣',
+          CityEngName: 'KinmenCounty'
+        },
+        {
+          CityName: '桃園市',
+          CityEngName: 'Taoyuan'
+        },
+        {
+          CityName: '臺北市',
+          CityEngName: 'Taipei'
+        },
+        {
+          CityName: '高雄市',
+          CityEngName: 'Kaohsiung'
+        },
+        {
+          CityName: '臺南市',
+          CityEngName: 'Tainan'
+        },
+        {
+          CityName: '嘉義市',
+          CityEngName: 'Chiayi'
+        }
+      ],
+      zoom: 15,
       tileLayerClass: (url, options) => L.mapboxGL(options),
       layerOptions: {
         accessToken:
@@ -82,10 +190,112 @@ export default {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         tileSize: 512,
-        zoomOffset: -1
+        zoomOffset: -1,
+        iconSize: [30, 40],
+        iconAnchor: [15, 0]
+      },
+      markerData: [],
+      citySelect: 'Taipei',
+      serviceSelect: 'rent',
+      dataLoaded: false,
+      keyWord: ''
+    }
+  },
+  methods: {
+    updateMarker () {
+      this.markerData = []
+      for (const data of this.stationsData) {
+        const markers = {
+          UID: data.StationUID,
+          position: [
+            data.StationPosition.PositionLat,
+            data.StationPosition.PositionLon
+          ],
+          address: data.StationAddress.Zh_tw,
+          name: data.StationName.Zh_tw,
+          updateTime: format(new Date(data.UpdateTime), 'yyyy-MM-dd HH:mm:ss'),
+          serviceType: data.ServiceType,
+          serviceStatus: data.ServiceStatus,
+          avaRent: data.AvailableRentBikes,
+          avaReturn: data.AvailableReturnBikes
+        }
+        const { avaRent, avaReturn } = markers
+        if (avaRent <= 5 && avaRent > 0) {
+          markers.rentIcon = 'bg-green-200'
+        } else if (avaRent > 5) {
+          markers.rentIcon = 'bg-green'
+        } else if (avaRent === 0) {
+          markers.rentIcon = 'bg-danger'
+        }
+        if (avaReturn <= 5 && avaReturn > 0) {
+          markers.returnIcon = 'bg-green-200'
+        } else if (avaReturn > 5) {
+          markers.returnIcon = 'bg-green'
+        } else if (avaReturn === 0) {
+          markers.returnIcon = 'bg-danger'
+        }
+        this.markerData.push(markers)
+      }
+      this.$store.dispatch('setIsLoading', false)
+    },
+    loadCityStationsData () {
+      this.$store.dispatch('setIsLoading', true)
+      return this.$store
+        .dispatch('station/fetchCityStationsData', {
+          city: this.citySelect,
+          keyword: this.keyWord
+        })
+        .then(() => {})
+        .catch((err) => {
+          this.$store.dispatch('setIsLoading', false)
+          console.log(err)
+        })
+    },
+    iconColor (serviceStatus, rentIcon, returnIcon) {
+      if (this.serviceSelect === 'rent') {
+        if (serviceStatus !== 1) {
+          return require('@/assets/images/gray-2-marker.png')
+        }
+        switch (rentIcon) {
+          case 'bg-green-200':
+            return require('@/assets/images/green-2-marker.png')
+          case 'bg-green':
+            return require('@/assets/images/green-marker.png')
+          case 'bg-danger':
+            return require('@/assets/images/error-marker.png')
+          default:
+            break
+        }
+      } else {
+        if (serviceStatus !== 1) {
+          return require('@/assets/images/gray-2-marker.png')
+        }
+        switch (returnIcon) {
+          case 'bg-green-200':
+            return require('@/assets/images/green-2-marker.png')
+          case 'bg-green':
+            return require('@/assets/images/green-marker.png')
+          case 'bg-danger':
+            return require('@/assets/images/error-marker.png')
+          default:
+            break
+        }
       }
     }
   },
-  methods: {}
+  created () {
+    this.loadCityStationsData()
+  },
+  watch: {
+    stationsData: {
+      deep: true,
+      handler () {
+        this.updateMarker()
+      }
+    },
+    noData (val) {
+      if (val) alert('查無資料')
+    }
+  }
 }
 </script>
